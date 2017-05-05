1536c15a84b71410873d78138d27e00c7040b6fc
dvsidorov <dvsidorov88@mail.ru>
2017-05-05 23:02:52 +0500

    Pages

--- papers/base/blank/views.py
+++ papers/base/blank/views.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 
+import os
 from django.shortcuts import render, \
     get_object_or_404, redirect
 
@@ -237,4 +238,92 @@ class ChunkInsideView(ChunkBaseView, ChunkParamsValidatorMixin, ExtraMixin, Conv
         return render(
                 self.request,
                 self.TEMPLATE,
+                self.output_context)
+
+
+class ChunkInsideTemplateView(ChunkBaseView, ChunkParamsValidatorMixin, ExtraMixin, ConverterMixin):
+    """ Chunk Template Inside View. Receives 
+        get params and response neither 
+        arguments in get request params.
+
+        BREADCRUMB_TITLE - title for breadcrumb
+        CHUNK_GRID_COUNT - count of grid chunks.
+        CATEGORY_MODEL - class of db "category" model.
+        CHUNK_MODEL - class of db "chunk" model.
+        TEMPLATE - class of db "template" model.
+    """
+    BREADCRUMB_TITLE = ''
+    CATEGORY_MODEL = None
+    CHUNK_MODEL = None
+    TEMPLATE_PATH = None
+    TEMPLATE_NAME = None
+
+    kwargs_params_slots = {
+        'chunk_slug_title': [None, ''],
+        'use_category': [None, True]
+    }
+
+    request_params_slots = {
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.params_storage = {}
+        self.output_context = {
+            'menu_sidebar': None,
+            'paper_inside': None,
+            'breadcrumb_page': None,
+        }
+        super(ChunkInsideTemplateView, self).__init__(*args, **kwargs)
+        self.category_input_s = list()
+        self.category_s = list()
+        self.menu_sidebar = {}
+        self.paper_inside = {}
+        self.breadcrumb_page = {}
+        self.current_category = None
+        self.parent_current_category = None
+
+    def _set_chunk(self):
+        self.chunk = get_object_or_404(self.CHUNK_MODEL,
+                                       slug_title=self.kwargs['chunk_slug_title'])
+
+    def _category_s_query(self):
+        if self.params_storage['use_category']:
+            self.category_s = self.CATEGORY_MODEL.get_root_nodes()
+            self.current_category = self.chunk.category
+            self.parent_current_category = self.current_category.get_parent()
+
+    def _set_chunk_image(self):
+        self.chunk.image_current = self.chunk.image
+
+    def _set_chunk_attach_image_s(self):
+        self.chunk.attach_images = self.chunk.chunk_attachment.filter(meaning=1).all()
+
+    def _set_chunk_attach_file_s(self):
+        self.chunk.attach_files = self.chunk.chunk_attachment.filter(meaning=0).all()
+
+    def _set_template(self):
+        slug_title = self.kwargs['chunk_slug_title']
+        self.template = os.path.join(self.TEMPLATE_PATH,
+                                     '{}__inside.html'.format(slug_title
+                                                              if slug_title
+                                                              else self.TEMPLATE_NAME))
+
+    def get(self, *args, **kwargs):
+        self._set_chunk()
+        self._set_chunk_image()
+        self._set_chunk_attach_image_s()
+        self._set_chunk_attach_file_s()
+        self._set_template()
+        self._category_s_query()
+        self._set_dispatcher()
+        self._set_labels()
+
+        self._format_breadcrumbs()
+        self._format_sidebar()
+        self._format_center_inside()
+
+        self._aggregate()
+        return render(
+                self.request,
+                self.template,
                 self.output_context)
